3ad70d75b69d4cb3d1319b01c232d646e03a4237
michaelignat <michael.ignat13@gmail.com>
2024-10-16 21:22:18 +0800

    feat: add dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx"],
-
-  /**
-   * GitHub Pages Configuration
-   *
-   * @see https://github.com/gregrickaby/nextjs-github-pages
-   */
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  swcMinify: true,
+  output: "standalone",
   reactStrictMode: true,
 };
 
